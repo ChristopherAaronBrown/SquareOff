7c81300acd420dced0f3cfa586891cfe29465ab3
Chris Brown <christopheraaronbrown@gmail.com>
2018-09-08 21:04:12 -0700

    .gitignore is now working

--- Pods/Target Support Files/Pods-Square Off/Pods-Square Off-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Square_Off : NSObject
-@end
-@implementation PodsDummy_Pods_Square_Off
-@end
--- Square Off/Square Off-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <Shimmer/FBShimmering.h>
-#import <Shimmer/FBShimmeringLayer.h>
-#import <Shimmer/FBShimmeringView.h>
